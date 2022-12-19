048053dd368ff78e289e9c495031493d5e0f542e
Viviane <vivianedel@gmail.com>
2022-12-19 17:50:57 -0300

    Add movies caminho

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const userRoutes = require('./routes/userRoutes');
-//const moviesRoutes = require('./routes/moviesRoutes');
+const moviesRoutes = require('./routes/moviesRoutes');
 
 const app = express();
 const port = 3030;
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 app.use('/v1/user', userRoutes);
-//app.use('/v2/user', moviesRoutes);
+app.use('/v2/movies', moviesRoutes);
 
 app.listen(port, () => {
     console.log('Server started on port: ' + port)
