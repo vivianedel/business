e890b2e3228ace02751f18881a4186f0be718b2f
Viviane <vivianedel@gmail.com>
2022-12-19 16:04:54 -0300

    Add movies route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const userRoutes = require('./routes/userRoutes');
+//const moviesRoutes = require('./routes/moviesRoutes');
 
 const app = express();
 const port = 3030;
@@ -12,6 +13,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 app.use('/v1/user', userRoutes);
+//app.use('/v2/user', moviesRoutes);
 
 app.listen(port, () => {
     console.log('Server started on port: ' + port)
---
+++ controllers/moviesController.js
@@ -0,0 +1,20 @@
+const userService = require('../services/userService');
+
+//
+const moviesController = {
+
+    getMovies: async (req, res) => {
+        try {
+            await moviesService.getMovies(req, res);
+        } catch (error) {
+            return res.status(500).send({
+                date: new Date(),
+                code: 500,
+                message: error
+            });
+        }
+    },
+
+}
+
+module.exports = moviesController;
