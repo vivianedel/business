6ae7a0bb098fa91a193626736a21ed3c8794028a
Eli Melo <elimelo@natura.net>
2023-01-02 20:40:21 -0300

    adding user registration

--- sql/user.sql
+++ sql/user.sql
@@ -5,7 +5,7 @@ CREATE TABLE Users (
     last_name varchar(255) NOT NULL,
     birth_date date NOT NULL,
     email varchar(255) NOT NULL,
-    categories_ids varchar(255),
+    categories_ids SETz,
     password varchar(255) NOT NULL,
     PRIMARY KEY (user_id)	
 );
